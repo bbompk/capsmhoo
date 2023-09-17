66ebd9b844401137da890691138ca156b7afe156
bbompk <panithiv@gmail.com>
2023-09-17 18:30:12 +0700

    feat: added and setup postgres container

---
+++ server/db/postgres_init.sql
@@ -0,0 +1,55 @@
+-- Create a new database
+-- CREATE DATABASE capsmhoo;
+
+-- Switch to the new database
+-- \c capsmhoo;
+
+-- Grant privileges
+-- ALTER ROLE 'admin' SET client_encoding TO 'utf8';
+-- ALTER ROLE 'admin' SET default_transaction_isolation TO 'read committed';
+-- ALTER ROLE 'admin' SET timezone TO 'UTC';
+-- GRANT ALL PRIVILEGES ON DATABASE capsmhoo TO 'admin';
+
+-- SET default_tablespace = '';
+-- SET default_table_access_method = heap;
+
+
+-- Create a "user" table
+CREATE TABLE users (
+    id VARCHAR(255) PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+-- Create a "professor" table with a foreign key to the "user" table
+CREATE TABLE professors (
+    id VARCHAR(255) PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    profile TEXT,
+    user_id VARCHAR(255) REFERENCES users(id)
+);
+
+-- Create a "team" table
+CREATE TABLE teams (
+    id VARCHAR(255) PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    profile TEXT
+);
+
+-- Create a "professor" table with a foreign key to the "user" and "team" table
+CREATE TABLE students (
+    id VARCHAR(255) PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    user_id VARCHAR(255) REFERENCES users(id),
+    team_id VARCHAR(255) REFERENCES teams(id)
+);
+
+CREATE TABLE project (
+    project_id VARCHAR(255) PRIMARY KEY,
+    team_id VARCHAR(255) REFERENCES teams(id),
+    professor_id VARCHAR(255) REFERENCES users(id),
+    name VARCHAR(255),
+    description TEXT
+);
+
+
