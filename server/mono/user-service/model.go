0846ddee99401623f10807a2f90f5c4f54551a45
sirapoom leesuravanich <sirapoomlee@gmail.com>
2023-09-21 07:33:02 +0700

    init student and professor service

--- server/mono/user-service/model.go
+++ server/mono/user-service/model.go
@@ -5,3 +5,17 @@ type User struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
+
+type Student struct {
+	ID     string  `json:"id"`
+	Name   string  `json:"name"`
+	TeamID *string `json:"team_id,omitempty"`
+	UserID string  `json:"user_id"`
+}
+
+type Professor struct {
+	ID      string `json:"id"`
+	Name    string `json:"name"`
+	Profile string `json:"profile"`
+	UserID  string `json:"user_id"`
+}
---
+++ server/mono/user-service/student_handler.go
@@ -0,0 +1,143 @@
+package user
+
+import (
+	"capsmhoo/common"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Define Dependencies
+type StudentHandler struct {
+	repo     StudentRepository
+	userrepo UserRepository
+}
+
+// Define what this will do
+type StudentHttpHandler interface {
+	GetStudents(c *gin.Context)
+	GetStudentByID(c *gin.Context)
+	CreateStudent(c *gin.Context)
+	UpdateStudentByID(c *gin.Context)
+	DeleteStudentByID(c *gin.Context)
+}
+
+func (h *StudentHandler) GetStudent(c *gin.Context) {
+	student := h.repo.GetStudents()
+
+	c.JSON(200, common.HttpResponse{
+		Code: "200",
+		Data: student,
+	})
+}
+func (h *StudentHandler) GetStudentByID(c *gin.Context) {
+	id := c.Param("id")
+	student, err := h.repo.GetStudentByID(id)
+	if err != nil {
+		c.JSON(200, common.HttpResponse{
+			Code: "400",
+			// Data: {},
+			Error: err.Error(),
+		})
+		return
+	}
+	c.JSON(200, common.HttpResponse{
+		Code: "200",
+		Data: student,
+	})
+}
+func (h *StudentHandler) CreateStudent(c *gin.Context) {
+	type Params struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+		Name     string `json:"name"`
+	}
+	var params Params
+	var student Student
+	var user User
+	if err := c.ShouldBindJSON(&params); err != nil {
+		c.JSON(200, common.HttpResponse{
+			Code: "400",
+			// Data: {},
+			Error: "Couldn't bind input to json",
+		})
+		return
+	}
+	user.Email = params.Email
+	user.Password = params.Password
+	student.Name = params.Name
+	createdUser, err := h.userrepo.CreateUser(user)
+	if err != nil {
+		c.JSON(200, common.HttpResponse{
+			Code: "400",
+			// Data: {},
+			Error: "User cannot be created",
+		})
+		return
+	}
+	// time.Sleep(time.Second)
+	student.UserID = createdUser.ID
+	createdStudent, err := h.repo.CreateStudent(student)
+	if err != nil {
+		c.JSON(200, common.HttpResponse{
+			Code: "400",
+			// Data: {},
+			Error: err.Error(),
+		})
+		return
+	}
+	// student := h.repo.CreateStudent()
+
+	c.JSON(200, common.HttpResponse{
+		Code: "200",
+		Data: createdStudent,
+	})
+}
+func (h *StudentHandler) UpdateStudentByID(c *gin.Context) {
+	id := c.Param("id")
+	var student Student
+	if err := c.ShouldBindJSON(&student); err != nil {
+		c.JSON(200, common.HttpResponse{
+			Code: "400",
+			// Data: {},
+			Error: err.Error(),
+		})
+		return
+	}
+	updatedStudent, err := h.repo.UpdateStudentByID(id, student)
+	if err != nil {
+		c.JSON(200, common.HttpResponse{
+			Code: "400",
+			// Data: {},
+			Error: err.Error(),
+		})
+		return
+	}
+	c.JSON(200, common.HttpResponse{
+		Code: "200",
+		Data: updatedStudent,
+	})
+}
+func (h *StudentHandler) DeleteStudentByID(c *gin.Context) {
+	id := c.Param("id")
+	err := h.repo.DeleteStudentByID(id)
+	if err != nil {
+		c.JSON(200, common.HttpResponse{
+			Code: "400",
+			// Data: {},
+			Error: err.Error(),
+		})
+		return
+	}
+	c.JSON(200, common.HttpResponse{
+		Code: "200",
+		Data: "",
+	})
+}
+
+// Dependency Injection
+func ProvideStudentHandler(repo StudentRepository, userrepo UserRepository) *StudentHandler {
+	return &StudentHandler{
+		repo:     repo,
+		userrepo: userrepo,
+	}
+}
