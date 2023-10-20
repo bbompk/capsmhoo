6262213691cd56c5c5c4ce329e94806a68f0ab4d
Akira Sitdhikariyawat <akira@PaPis-MacBook-Pro.local>
2023-10-20 14:36:51 +0700

    add model

--- server/mono/api-gateway/model/model.go
+++ server/mono/api-gateway/model/model.go
@@ -18,3 +18,23 @@ type Notification struct {
 	UserID string `json:"user_id"`
 	IsRead bool   `json:"is_read"`
 }
+
+type User struct {
+	ID       string `json:"id"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type Student struct {
+	ID     string  `json:"id"`
+	Name   string  `json:"name"`
+	TeamID *string `json:"team_id,omitempty"`
+	UserID string  `json:"user_id"`
+}
+
+type Professor struct {
+	ID      string `json:"id"`
+	Name    string `json:"name"`
+	Profile string `json:"profile"`
+	UserID  string `json:"user_id"`
+}
