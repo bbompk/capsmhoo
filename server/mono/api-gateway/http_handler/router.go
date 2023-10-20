62529b0bde8baf7e4a4c8863bc5b1aa325a772a1
Akira Sitdhikariyawat <akira@PaPis-MacBook-Pro.local>
2023-10-20 14:37:29 +0700

    setup router

--- server/mono/api-gateway/http_handler/router.go
+++ server/mono/api-gateway/http_handler/router.go
@@ -8,8 +8,13 @@ func ProvideRouter(
 	r *gin.Engine,
 	teamHandler ITeamHandler,
 	teamJoinRequestHandler ITeamJoinRequestHandler,
+	userHandler IUserHandler,
+	studentHandler IStudentHandler,
+	professorHandler IProfessorHandler,
+	projectHandler IProjectHandler,
 	notiHandler INotiHandler,
 ) {
+	// team service
 	r.GET("/team", teamHandler.GetAllTeams)
 	r.GET("/team/:id", teamHandler.GetTeamByID)
 	r.POST("/team", teamHandler.CreateTeam)
@@ -18,6 +23,32 @@ func ProvideRouter(
 	r.POST("/team/add-student/:id", teamHandler.AddStudentToTeam)
 	r.POST("/team/remove-student/:id", teamHandler.RemoveStudentFromTeam)
 
+	// user service
+	r.GET("/user/:id", userHandler.GetUserByID)
+	r.GET("/user", userHandler.GetUser)
+	r.POST("/user", userHandler.CreateUser)
+	r.PUT("/user/:id", userHandler.UpdateUserByID)
+	r.DELETE("/user/:id", userHandler.DeleteUserByID)
+	r.DELETE("/user", userHandler.DeleteAll)
+
+	r.GET("/student/:id", studentHandler.GetStudentByID)
+	r.GET("/student", studentHandler.GetStudent)
+	r.POST("/student", studentHandler.CreateStudent)
+	r.PUT("/student/:id", studentHandler.UpdateStudentByID)
+	r.DELETE("/student/:id", studentHandler.DeleteStudentByID)
+	r.DELETE("/student", studentHandler.DeleteStudentAll)
+
+	r.GET("/professor/:id", professorHandlerHandler.GetProfessorByID)
+	r.GET("/professor", professorHandlerHandler.GetProfessor)
+	r.POST("/professor", professorHandlerHandler.CreateProfessor)
+	r.PUT("/professor/:id", professorHandlerHandler.UpdateProfessorByID)
+	r.DELETE("/professor/:id", professorHandlerHandler.DeleteProfessorByID)
+	r.DELETE("/professor", professorHandlerHandler.DeleteProfessorAll)
+
+	// project service
+	// todo: wait for project service
+
+	// noti service
 	notiRoute := r.Group("/noti")
 	notiRoute.GET("/:id", notiHandler.GetAllNotiByUserId)
 	notiRoute.POST("/:id", notiHandler.ReadNoti)
