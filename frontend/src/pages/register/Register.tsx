a3d5c6d96d65a6d95b196417b816df6331a44efb
AkiraSitdhi <ppoonpy@gmail.com>
2023-11-06 02:18:27 +0700

    register page styling

--- frontend/src/pages/register/Register.tsx
+++ frontend/src/pages/register/Register.tsx
@@ -57,39 +57,136 @@ enum UserType {
     };
   
     return (
-      <>
-        {/* Toggle buttons for UserType */}
-        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                    Register your account
-                </h2>
+        <>
+          <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-md">
+              <h2 className="mt-10 text-center text-3xl font-extrabold text-gray-900">
+                Register your account
+              </h2>
+              <div className="mt-8 flex justify-center">
+                <button
+                  type="button"
+                  onClick={() => setUserType(UserType.Student)}
+                  className={`px-4 py-2 rounded-md ${userType === UserType.Student ? 'bg-indigo-600 text-white' : 'bg-white text-indigo-600'}`}
+                >
+                  Student
+                </button>
+                <button
+                  type="button"
+                  onClick={() => setUserType(UserType.Professor)}
+                  className={`px-4 py-2 rounded-md ml-4 ${userType === UserType.Professor ? 'bg-indigo-600 text-white' : 'bg-white text-indigo-600'}`}
+                >
+                  Professor
+                </button>
+              </div>
+              <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
+                <div>
+                  <label htmlFor="name" className="block text-sm font-medium text-gray-700">
+                    Name
+                  </label>
+                  <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    required
+                    placeholder="Your name"
+                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    required
+                    placeholder="Your email"
+                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    required
+                    placeholder="Your password"
+                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </div>
+                {userType === UserType.Professor && (
+                  <div>
+                    <label htmlFor="profile" className="block text-sm font-medium text-gray-700">
+                      Profile
+                    </label>
+                    <textarea
+                      name="profile"
+                      id="profile"
+                      required
+                      placeholder="Your profile description"
+                      className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                      value={profile}
+                      onChange={(e) => setProfile(e.target.value)}
+                    />
+                  </div>
+                )}
+                <button
+                  type="submit"
+                  className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                >
+                  Register as {userType}
+                </button>
+              </form>
             </div>
-            <div className="flex min-h-full flex-1 flex-row justify-center px-6 py-12 lg:px-8 space-x-8 user-type-toggle">
-                <button onClick={() => setUserType(UserType.Student)}>Student</button>
-                <button onClick={() => setUserType(UserType.Professor)}>Professor</button>
-            </div>
-            <div className="flex min-h-full flex-1 flex-col justify-center">
-                <form onSubmit={handleSubmit} className="flex min-h-full flex-1 flex-col justify-center">
-                    {/* Shared fields between Student and Professor */}
-                    <input type="text" value={name} onChange={(e) => setName(e.target.value)} required placeholder="Name"/>
-                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required placeholder="Email" />
-                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required placeholder="Password" />
-                        {/* Professor-specific field */}
-                        {userType === UserType.Professor && (
-                        <input type="text" value={profile} onChange={(e) => setProfile(e.target.value)} required placeholder="Profile" />
-                    )}
-                    <button type="submit"
-                        className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                    >
-                        Register as {userType}
-                    </button>
-                    {/* <button type="submit">Register as {userType}</button> */}
-                </form>
-            </div>
-        </div>
-      </>
-    );
+          </div>
+        </>
+      );
+ 
+//     return (
+//       <>
+//         {/* Toggle buttons for UserType */}
+//         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+//             <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+//                 <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+//                     Register your account
+//                 </h2>
+//             </div>
+//             <div className="flex min-h-full flex-1 flex-row justify-center px-6 py-12 lg:px-8 space-x-8 user-type-toggle">
+//                 <button onClick={() => setUserType(UserType.Student)}>Student</button>
+//                 <button onClick={() => setUserType(UserType.Professor)}>Professor</button>
+//             </div>
+//             <div className="flex min-h-full flex-1 flex-col justify-center">
+//                 <form onSubmit={handleSubmit} className="flex min-h-full flex-1 flex-col justify-center">
+//                     {/* Shared fields between Student and Professor */}
+//                     <input type="text" value={name} onChange={(e) => setName(e.target.value)} required placeholder="Name"/>
+//                     <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required placeholder="Email" />
+//                     <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required placeholder="Password" />
+//                         {/* Professor-specific field */}
+//                         {userType === UserType.Professor && (
+//                         <input type="text" value={profile} onChange={(e) => setProfile(e.target.value)} required placeholder="Profile" />
+//                     )}
+//                     <button type="submit"
+//                         className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+//                     >
+//                         Register as {userType}
+//                     </button>
+//                     {/* <button type="submit">Register as {userType}</button> */}
+//                 </form>
+//             </div>
+//         </div>
+//       </>
+//     );
   };
-  
+
   export default Register;
