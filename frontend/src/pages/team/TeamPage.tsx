f0e1556f1ac37c58caac095cb21df2c0d223b16b
Peeranut "Max" Kit <peeranut.kit.work@gmail.com>
2023-11-09 22:13:09 +0700

    Fix error on TeamPage

--- frontend/src/pages/team/TeamPage.tsx
+++ frontend/src/pages/team/TeamPage.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
-import { TeamInterface } from "../../interfaces/TeamInterface";
+import { TeamInterface, TeamJoinRequestInterface } from "../../interfaces/TeamInterface";
 import { useParams } from "react-router-dom";
+import { createTeamJoinRequest } from "../../service/TeamJoinRequestService";
 
 const TeamPage = () => {
   const { id } = useParams();
@@ -17,14 +18,29 @@ const TeamPage = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    const teamJoinRequest : TeamJoinRequestInterface = {
+      id: "",
+      team_id: JSON.stringify(data?.id),
+      student_id: ""
+    }
+    
+    e.preventDefault();
+    try {
+        await createTeamJoinRequest(teamJoinRequest);
+    } catch (error) {
+      console.error(error);
+      return;
+    }
+  }
+
   return (
     <div>
       <div className="min-h-screen">
         <h1 className=" text-center text-3xl p-4">Team Page</h1>
         <h2>Name: {data?.name}</h2>
         <p>Profile: {data?.profile}</p>
-        <form action="http://localhost:8082/team-join-request" method="POST">
-          <input type="hidden" name="teamID" value={data?.id} />
+        <form onSubmit={handleSubmit}>
           <button type="submit" className="btn btn-primary">
             Request to Join
           </button>
