757296f691d5ef43718763e877cae8d1b68c9f6e
bbompk <panithiv@gmail.com>
2023-11-10 21:10:43 +0700

    auth provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 import Navbar from './components/header/Navbar';
 import Home from './pages/home/Home';
-import CreateProject from './pages/form/CreateProject';
 import CreateTeam from './pages/form/CreateTeam';
 import ProjectList from './pages/proj/ProjectList';
 import TeamList from './pages/team/TeamList';
@@ -27,16 +26,14 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path='/register' element={<Register/>} />
           <Route path='/login' element={<Login />} />
-          {/* <Route path='/profile' element={<AuthProvider><Profile/></AuthProvider>} /> */}
-          <Route path='/profile' element={<Profile/>} />
-          {/* <Route path='/create-project' element={<CreateProject />} /> */}
-          <Route path='/create-team' element={<CreateTeam />} />
+          <Route path='/profile' element={<AuthProvider><Profile/></AuthProvider>} />
+          <Route path='/create-team' element={<AuthProvider><CreateTeam /></AuthProvider>} />
           <Route path='/view-project' element={<AuthProvider><ProjectList/></AuthProvider>} />
-          <Route path='/view-team' element={<TeamList/>} />
-          <Route path='/team-detail/:id' element={<TeamPage/>} />
-          <Route path='/my-team' element={<MyTeam/>} />
-          <Route path='/project-detail/:id' element={<ProjectPage/>} />
-          <Route path='/join-request' element={<TeamJoinRequest/>} />
+          <Route path='/view-team' element={<AuthProvider><TeamList/></AuthProvider>} />
+          <Route path='/team-detail/:id' element={<AuthProvider><TeamPage/></AuthProvider>} />
+          <Route path='/my-team' element={<AuthProvider><MyTeam/></AuthProvider>} />
+          <Route path='/project-detail/:id' element={<AuthProvider><ProjectPage/></AuthProvider>} />
+          <Route path='/join-request' element={<AuthProvider><TeamJoinRequest/></AuthProvider>} />
           <Route path='/private' element={<AuthProvider><h1>Private</h1></AuthProvider>} />
           <Route path='/mock-login' element={<MockLogin />} />
           </Routes>
